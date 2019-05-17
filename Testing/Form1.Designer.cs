d059812bbd6519623f4531df4be41ea3d1f55944
yuguochao <yugc@mesnac.com>
2019-05-17 09:26:28 +0800

    1

--- Testing/Form1.Designer.cs
+++ Testing/Form1.Designer.cs
@@ -278,7 +278,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(31, 134);
+            this.button1.Location = new System.Drawing.Point(32, 150);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(151, 37);
             this.button1.TabIndex = 27;
